9f0bb19e72118cbf9705791f59bcf0dfa71dd42b
trchristensen-99 <trchristensen99@gmail.com>
2026-02-25 15:43:43 -0500

    Fix SLURM time limit: 12h for gpuq partition (was 24h which exceeds QoS limit)

--- scripts/slurm/train_ag_yeast_flatten_512_256.sh
+++ scripts/slurm/train_ag_yeast_flatten_512_256.sh
@@ -6,7 +6,7 @@
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
-#SBATCH --time=24:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=christen@cshl.edu
 
